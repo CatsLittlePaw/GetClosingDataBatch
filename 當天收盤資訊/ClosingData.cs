a20e2a49026815a1cf09b709440ab7d94d6f9c7e
WhoIWannaBe <atheismy1998@gmail.com>
2021-09-30 23:49:18 +0800

    初版

---
+++ 當天收盤資訊/ClosingData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataTemplate
+{
+    public class ClosingData
+    {
+        public string STOCK_CODE { get; set; }
+        public string TS { get; set; }
+        public string VOLUME { get; set; }
+        public string CLOSE_PRICE { get; set; }
+        public string BID_PRICE { get; set; }
+        public string BID_VOLUME { get; set; }
+        public string ASK_PRICE { get; set; }
+        public string ASK_VOLUME { get; set; }
+
+        public void SetProperties(string code, string ts, string volume, string close, string bid_price, string bid_volume, string ask_price, string ask_volume)
+        {
+            this.STOCK_CODE = code;
+            this.TS = ts;
+            this.VOLUME = volume;
+            this.CLOSE_PRICE = close;
+            this.BID_PRICE = bid_price;
+            this.BID_VOLUME = bid_volume;
+            this.ASK_PRICE = ask_price;
+            this.ASK_VOLUME = ask_volume;
+        }
+    }
+}
---
+++ 當天收盤資訊/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using SQLExtend;
+using DataTemplate;
+using log4net;
+using Sinopac.Shioaji;
+
+
+// 這裡的Config，因為不能透過ConfigManager抓，使用相對路徑
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = @"當天收盤資訊.dll.config", Watch = true)]
+namespace ShioajiConsole
+{
+
+    class Program
+    {
+        public static ILog log = LogManager.GetLogger("logger");
+
+        static void Main(string[] args)
+        {
+            log.Debug("排程「當天收盤資訊」 開始執行");
+
+            
+            MyApi ma = new MyApi();
+            ma.Login();
+            log.Debug("Shioaji 登入成功");
+            ma.getTodayClosingData();
+            
+            log.Debug("排程「當天收盤資訊」 執行完畢");
+        }
+    }
+
+    public class MyApi
+    {
+        private static Shioaji _api = new Shioaji();
+        private static SJList _accounts;
+        public void Login()
+        {
+            string personal_id = ConfigurationManager.AppSettings["account"].ToString();
+            string pwd = ConfigurationManager.AppSettings["password"].ToString();
+            _accounts = _api.Login(personal_id, pwd);
+        }
+        public void getTodayClosingData(string type = "TSE")
+        {
+            string StockCodes = ConfigurationManager.AppSettings["StockCodes"].ToString();
+            string[] stockCodeList = StockCodes.Split(",");
+            
+            for(var i = 0; i < stockCodeList.Length; ++i)
+            {
+                var contract = _api.Contracts.Stocks[type][stockCodeList[i]];                
+                Ticks ticks = _api.Ticks(contract, DateTime.Now.ToString("yyyy-MM-dd"));
+                
+                
+                ClosingData data = new ClosingData();
+                data.SetProperties(stockCodeList[i] , ticks.ts[0].ToString(), ticks.volume[0].ToString(), ticks.close[0].ToString(), ticks.bid_price[0].ToString(), ticks.bid_volume[0].ToString(), ticks.ask_price[0].ToString(), ticks.ask_volume[0].ToString());
+                ADODB.Insert<ClosingData>(data);                
+            }
+        }
+    }
+}
+
